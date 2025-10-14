c8d388f25a9c2dbc82c228d3485f1aa3b19b2ebf
James Harton <james@harton.nz>
2025-10-15 09:06:35 +1300

    chore: Fix REUSE copyright attributions

--- test/support/api.ex
+++ test/support/api.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020 Zach Daniel
+# SPDX-FileCopyrightText: 2022 ash_blog contributors <https://github.com/ash-project/ash_blog/graphs.contributors>
 #
 # SPDX-License-Identifier: MIT
 
